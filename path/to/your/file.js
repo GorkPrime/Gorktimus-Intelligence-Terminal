377c0c5123f70df4f20d4e8cabf7c375f78470b1
Gorktimus Prime <gorktimusprime@gmail.com>
2026-03-28 10:10:44 -0500

    Update showMainMenu function to call getDevModeStatus() properly

---
+++ path/to/your/file.js
@@ -0,0 +1,5 @@
+function showMainMenu() {
+    const devModeStatus = getDevModeStatus();
+    const caption = `Main Menu ${devModeStatus ? '(Dev Mode Enabled)' : '(Dev Mode Disabled)'}`;
+    // other code...
+}
